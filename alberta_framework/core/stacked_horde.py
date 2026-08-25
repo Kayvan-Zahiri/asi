5257d2b04008c9c3e5a14ae901b110dc220b5ad0
kzahiri1 <kzahiri@dons.usfca.edu>
2026-08-24 17:05:06 -0700

    fix(core): derive _ACTUAL_INT_TYPES from dtype codes in stacked_horde and reward_model

--- alberta_framework/core/stacked_horde.py
+++ alberta_framework/core/stacked_horde.py
@@ -142,19 +142,7 @@ _CONFIG_FIELDS = {
     "step_size",
 }
 _ACTUAL_INT_TYPES = frozenset(
-    {
-        int,
-        np.int8,
-        np.int16,
-        np.int32,
-        np.int64,
-        np.uint8,
-        np.uint16,
-        np.uint32,
-        np.uint64,
-        np.longlong,
-        np.ulonglong,
-    }
+    {int, *(np.dtype(code).type for code in "bBhHiIlLqQpP")}
 )
 
 
